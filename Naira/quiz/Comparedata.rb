3725abda5a75c3f1aab1080eb8f5a3f38a4db705
naira88re <naira88re@gmail.com>
2016-05-13 17:06:01 -0400

    Quiz cucumber

---
+++ Naira/quiz/Comparedata.rb
@@ -0,0 +1,24 @@
+
+Given(/^A list of items$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I look an item whitefish$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^a mesage is diplay that says The is found$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a list of items$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I look an item redfish$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^a messge is diplay that says the item is not found$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ Naira/quiz/logine.rb
@@ -0,0 +1,26 @@
+Given(/^The log in form is loadded$/) do
+  puts "The form is displayed"
+end
+
+When(/^I write a (.*) into username fiel$/) do |username|
+  
+end
+
+When(/^I write (\d+) into password fiel$/) do |password|
+  puts "aaa"
+end
+
+When(/^I click on log in button$/) do
+ puts "fff"
+end
+
+Then(/^a message is displayed (.*)$/) do |mesage|
+  puts "test"
+end
+
+
+
+
+
+
+
